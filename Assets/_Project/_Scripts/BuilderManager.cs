c9c84bab65bb2e1bac16b6f34620ff1ea4d183c8
Maciej Góralczyk <goralczykmaciek@gmail.com>
2025-01-23 22:15:49 +0100

    Rework of building manager

--- Assets/_Project/_Scripts/BuilderManager.cs
+++ Assets/_Project/_Scripts/BuilderManager.cs
@@ -1,20 +1,29 @@
-﻿using MG_Utilities;
+﻿using System.Collections.Generic;
+using MG_Utilities;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace EternalDefenders
 {
     public class BuilderManager : Singleton<BuilderManager>
     {
-        [SerializeField] TowerController towerPrefab;
+        [SerializeField] List<TowerController> towers;
+        
         bool _isEnabled = false;
+        int _currentIndex = 0;
+        
+        //TODO connect to input manager
         void Update()
         {
             if(Input.GetKeyDown(KeyCode.B))
                 _isEnabled = !_isEnabled;
             
             if(!_isEnabled) return;
+            
+            if(Input.GetMouseButtonDown(1))
+                _currentIndex = (_currentIndex + 1) % towers.Count;
 
-            if (Input.GetMouseButtonDown(0))
+            if(Input.GetMouseButtonDown(0))
             {
                 HexTile tile = HexMapController.Instance.GetHexTileFromWorldPosition(
                     CameraController.Instance.GetWorldMousePosition()
@@ -22,7 +31,7 @@ namespace EternalDefenders
 
                 if (tile.CanBuild())
                 {
-                    tile.BuildOnThisTile(towerPrefab);
+                    tile.BuildOnThisTile(towers[_currentIndex]);
                 }
             }
         }
