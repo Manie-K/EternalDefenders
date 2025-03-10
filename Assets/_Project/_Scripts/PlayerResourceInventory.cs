4f7f102f0e01eab4b3635c50eef813cae6334422
Maciej Góralczyk <goralczykmaciek@gmail.com>
2025-03-10 17:53:51 +0100

    Created resource inventory. Added tower costs.

---
+++ Assets/_Project/_Scripts/PlayerResourceInventory.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using MG_Utilities;
+using UnityEngine;
+
+namespace EternalDefenders
+{
+    public class PlayerResourceInventory : Singleton<PlayerResourceInventory>
+    {
+        readonly Dictionary<ResourceSO, int> _resources = new();
+     
+        public int GetResourceAmount(ResourceSO resourceType)
+        {
+            return _resources.ContainsKey(resourceType) ? _resources[resourceType] : 0;
+        }
+        
+        public void AddResource(ResourceSO resourceType, int amount)
+        {
+            if(amount <= 0) return;
+            if (!_resources.TryAdd(resourceType, amount))
+            {
+                _resources[resourceType] += amount;
+            }
+        }
+        
+        public int RemoveResource(ResourceSO resourceType, int amount)
+        {
+            if(amount <= 0) return 0;
+            if(!_resources.ContainsKey(resourceType))
+                return 0;
+            
+            
+            int amountToRemove = Mathf.Min(amount, _resources[resourceType]);
+            _resources[resourceType] -= amountToRemove;
+            if (_resources[resourceType] <= 0)
+            {
+                _resources.Remove(resourceType);
+            }
+            return amountToRemove;
+        }
+        
+        public bool HasEnoughOfResource(ResourceSO resourceType, int amount)
+        {
+            if(amount <= 0) return true;
+            
+            return _resources.ContainsKey(resourceType) && _resources[resourceType] >= amount;
+        }
+    }
+}
---
+++ Assets/_Project/_Scripts/ResourceSO.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace EternalDefenders
+{
+    [CreateAssetMenu(fileName = "ResourceSO", menuName = "EternalDefenders/Resource/ResourceSO")]
+    public class ResourceSO : ScriptableObject
+    {
+        public string Name;
+        public Sprite sprite;
+    }
+}
