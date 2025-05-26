9cd8e5a3977c0f07ffcd0bfd1a7a5c7d223f20a0
Maciej Góralczyk <goralczykmaciek@gmail.com>
2025-05-26 17:52:07 +0200

    Fix: Towers are removed from data field in HexTile class after being deleted.

--- Assets/_Project/_Scripts/Map/HexTile.cs
+++ Assets/_Project/_Scripts/Map/HexTile.cs
@@ -1,4 +1,5 @@
-﻿using MG_Utilities;
+﻿using System;
+using MG_Utilities;
 using UnityEngine;
 
 namespace EternalDefenders
@@ -10,8 +11,24 @@ namespace EternalDefenders
         
         public TowerBase Building { get; private set; }
         public float BuildingHeight => buildingHeight;
-        
+
+
+        void Start()
+        {
+            TowerController.OnTowerDestroyed += (destroyedTower =>
+            {
+                if(destroyedTower != null && destroyedTower == Building)
+                {
+                    ClearBuilding();
+                }
+            });
+        }
+
         public bool CanBuild() => canBuild && Building is null;
         public void SetBuilding(TowerBase building) => Building = building;
+        public void ClearBuilding()
+        {
+            Building = null;
+        }
     }
 }
